02c51052b376b52f2d93ba69f879a239b56f7224
Erbay Yigit <erbayyigit@gmail.com>
2021-08-06 10:14:53 -0400

    Update count_fastq_reads.sh

--- count_fastq_reads.sh
+++ count_fastq_reads.sh
@@ -3,3 +3,9 @@ for i in $1/*.1.fastq.gz
 do
    echo "$i" $(gunzip -c $i | echo `wc -l`/4 | bc -l)
 done
+
+#OR in the current folder, do this:
+for i in ./*.1.fastq.gz
+do
+   echo "$i" $(gunzip -c $i | echo `wc -l`/4 | bc -l)
+done
