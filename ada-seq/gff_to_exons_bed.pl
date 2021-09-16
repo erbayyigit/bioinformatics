41195d839bfb00161441d65119555804f0b5ccf8
Erbay Yigit <yigit@neb.com>
2021-09-15 21:52:43 -0400

    Update gff_to_exons_bed.pl

--- ada-seq/gff_to_exons_bed.pl
+++ ada-seq/gff_to_exons_bed.pl
@@ -4,8 +4,6 @@ use warnings;
 # Erbay Yigit, yigit@neb.com
 # Script: Exons start end BED file
 # Input file is a GFF file with exons
-
-
 while(<DATA>){
 	my $line = $_;
 	my($chr, $strand, $eStart, $eEnd, $gene)=(split /\s+/, $line)[0,1,5,6,7];
@@ -16,7 +14,7 @@ while(<DATA>){
 		say "$chr\t$pair\t$gene";
 	}
 }
-
+#chrom	strand	txStart	txEnd	exonCount	exonStarts	exonEnds	name2
 __DATA__
 chr11	-	1584341	1585283	1	1584341,	1585283,	KRTAP5-1
 chr11	-	1597176	1598294	1	1597176,	1598294,	KRTAP5-2
