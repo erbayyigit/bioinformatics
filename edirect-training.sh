3b3251cd1aa106adc9528bb57564eb1edea7db77
Erbay Yigit <yigit@neb.com>
2022-07-23 18:30:43 -0400

    Update edirect-training.sh

--- edirect-training.sh
+++ edirect-training.sh
@@ -1,1 +1,8 @@
 
+#Example-1
+#Finde most prolific authors who published on m6A
+esearch -db pubmed -query "m6A"|\
+efetch -format xml|\
+xtract -pattern PubmedArticle -block Author \
+-sep " " -tab "\n" -element LastName,Initials|\
+sort-uniq-count-rank
