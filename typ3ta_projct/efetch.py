74c5a151d87ed576b614f58052fe0adddc46fa4e
Erbay Yigit <yigit@neb.com>
2023-02-25 00:12:13 -0500

    Update efetch.py

--- typ3ta_projct/efetch.py
+++ typ3ta_projct/efetch.py
@@ -1,13 +1,7 @@
-'''
-It will fetch protein record based on genbank IDs.
-List of attributes can be found here.
-https://biopython.org/docs/1.74/api/Bio.GenBank.Record.html
-yigit@neb.com 
-'''
+from Bio import Entrez, SeqIO
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
-from Bio import SeqIO, Entrez
-
-temp_list=[]
+temp_list=[] # get list of genbank IDs srater with WP_
 with open('type3tas_accession_numbers.txt', 'r') as infile:
    for gb_id in infile:
       gb_id =gb_id.strip()
@@ -16,9 +10,16 @@ with open('type3tas_accession_numbers.txt', 'r') as infile:
 
 list_of_WP_ids = set(temp_list)      
 Entrez.email = "erbayyigit@gmail.com"
-
 handle = Entrez.efetch(db="protein", rettype="gb",  retmode="text", id=list_of_WP_ids)
+
 seq_records = list(SeqIO.parse(handle, "gb"))
 for record in seq_records:
-   print(record.id,  record.annotations['organism'], record.seq)
+   #print(record.id,  record.annotations['organism'], record.seq)
+   P = ProteinAnalysis(record.seq)
+   mw = f'{(P.molecular_weight()/1000):.2f}kDa'
+   isoep = f'{P.isoelectric_point():.2f}'
+   cystesins = P.count_amino_acids()['C']
+   charge=f'{P.charge_at_pH(7.0):.2f}'
+   
+   print(f'>{record.id} {mw} {isoep} pH7:{charge} Cys:{cystesins}\n{record.seq}')
 
