d9b234e355399c9c4d5e2ac7fdc63471a99ab8ff
Erbay Yigit <yigit@neb.com>
2023-05-20 16:07:19 -0400

    Update fuzzy-search.py
    Updated the script to read fasta records from a file.

--- motif-search/script/fuzzy-search.py
+++ motif-search/script/fuzzy-search.py
@@ -1,41 +1,29 @@
 ''' 
     #https://github.com/taleinat/fuzzysearch
-    This allows fuzzy search, for example find a motif with single mismatch.
-    
+    --This allows fuzzy search, for example find a motif with single 
+    mismatch.
+    --It read fasta file
     Alternatives recommended but I have not tried yet:
     https://github.com/Martinsos/edlib
-
-'''
-'''
     yigit@neb.com May 18, 20023
 '''
 
 # Example below.
 # Need to write from file version otherwise need to escape line breaks..
-
 from fuzzysearch import find_near_matches
+from Bio import SeqIO
 
-sequence= '''\
-CGAAATTAATACGACTCACTATAAGGCTTGCTTGTTCTTTTTGCAGAAGCTCAGAATAAAC
-GCTCAACTTTGGCACCAAAACATGGGAGTGCACGAGTGCCCCGCGTGGCTGTGGCTGCTGC
-TGAGCCTCCTGAGCCTCCCACTGGGACTGCCCGTGCTGGGGGCACCACCCAGACTGATCTG
-CGACAGCCGGGTACTGGAGAGGTACCTGCTGGAAGCCAAAGAAGCCGAGAATATCACAACC
-GGATGCGCCGAGCACTGCAGCCTCAACGAGAACATCACCGTACCGGACACAAAGGTCAACT
-TCTACGCATGGAAGAGAATGGAAGTAGGACAGCAGGCCGTCGAAGTGTGGCAGGGGCTCGC
-GCTGCTGAGCGAGGCGGTGCTGCGGGGCCAGGCCCTCCTCGTCAACAGCAGCCAGCCGTGG
-GAGCCCCTCCAACTGCACGTCGACAAAGCGGTGAGCGGGCTCCGCAGCCTGACGACGCTGC
-TGCGGGCCCTGGGCGCACAAAAGGAGGCCATCAGCCCGCCCGACGCGGCCAGCGCGGCACC
-CCTCCGAACGATCACCGCGGACACGTTCAGGAAGCTGTTCAGAGTGTACAGCAACTTCCTC
-CGCGGAAAGCTGAAACTGTACACCGGCGAAGCGTGCAGGACAGGGGACCGCTAGGACTGAC
-TAGGATCTGGTTACCACTAAACCAGCCTCAAGAACACCCGAATGGAGTCTCTAAGCTACATA
-ATACCAACTTACACTTTACAACATGTTGTCCCCCAAACTGTAGCCATTCGTATCTGCTCCTAATAA
-AAAGAAAGTTTCTTCACATTCTAGCTAG'''
+mismatch = 1
+input_file = 'inputfile.fasta'
+fasta_records = SeqIO.parse(input_file,'fasta')
 
-subsequence = 'AAAACAT' # distance = 1
-x=find_near_matches(subsequence, sequence, max_l_dist=1)
+for record in fasta_records:
+    name, sequence = record.id, str(record.seq)
+    subsequence = 'AAAACAT' # distance = 1
+    matches = find_near_matches(subsequence, sequence, max_l_dist=mismatch)
+    [print (name,'\t' ,item) for item in matches]
 
-print(x)
-print("---")
-print(x[0].start, x[0].matched)
 
+#print(x[0].start, x[0].matched)
 #print(sequence[x[0].start:])
+
