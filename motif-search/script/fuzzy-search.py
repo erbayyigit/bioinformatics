ef4da12031fbf0a3a7617f4180b531667b2f7733
Erbay Yigit <yigit@neb.com>
2023-05-22 20:48:25 -0400

    Update fuzzy-search.py
    Updated in-del options

--- motif-search/script/fuzzy-search.py
+++ motif-search/script/fuzzy-search.py
@@ -1,28 +1,33 @@
 ''' 
     #https://github.com/taleinat/fuzzysearch
-    --This allows fuzzy search, for example find a motif with single 
+    --This allows fuzzy search, for example it finds a motif with single 
     mismatch.
-    --It read fasta file
-    Alternatives recommended but I have not tried yet:
+    --It reads fasta file directly
+    Another alternative was recommended but I have not tried yet:
     https://github.com/Martinsos/edlib
-    yigit@neb.com May 18, 20023
-'''
+    yigit@neb.com May 21, 20023
+    USAGE:
+    max_deletions=1, "delete" = skip a character in the sub-sequence
+    max_insertions=1, maximum # of insertions ("insert" = skip a character in the sequence)
 
-# Example below.
-# Need to write from file version otherwise need to escape line breaks..
+'''
 from fuzzysearch import find_near_matches
 from Bio import SeqIO
 
-mismatch = 1
-input_file = 'inputfile.fasta'
+input_file = 'gey85-86-87.fasta'
 fasta_records = SeqIO.parse(input_file,'fasta')
 
 for record in fasta_records:
+    
     name, sequence = record.id, str(record.seq)
-    subsequence = 'AAAACAT' # distance = 1
-    matches = find_near_matches(subsequence, sequence, max_l_dist=mismatch)
-    [print (name,'\t' ,item) for item in matches]
-
+    subsequence = 'TGGTGT' # distance = 1
+    matches = find_near_matches(subsequence, sequence, 
+        max_l_dist=1, 
+        max_deletions=0,
+        max_insertions=0)
+    print("---record separator---")
+    [print (f"{name}\t{item}\t{len(record)}_nt") for item in enumerate(matches)]
+    
 
 #print(x[0].start, x[0].matched)
 #print(sequence[x[0].start:])
