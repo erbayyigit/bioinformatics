d635781702d6a689714af8e0564993c19c30e8db
Erbay Yigit <yigit@neb.com>
2023-05-22 22:52:25 -0400

    Update fuzzy-search.py
    Enumarated and updated printing format.

--- motif-search/script/fuzzy-search.py
+++ motif-search/script/fuzzy-search.py
@@ -14,19 +14,19 @@
 from fuzzysearch import find_near_matches
 from Bio import SeqIO
 
-input_file = 'gey85-86-87.fasta'
+input_file = 'out.fasta'
 fasta_records = SeqIO.parse(input_file,'fasta')
 
 for record in fasta_records:
-    
     name, sequence = record.id, str(record.seq)
     subsequence = 'TGGTGT' # distance = 1
     matches = find_near_matches(subsequence, sequence, 
         max_l_dist=1, 
         max_deletions=0,
         max_insertions=0)
-    print("---record separator---")
-    [print (f"{name}\t{item}\t{len(record)}_nt") for item in enumerate(matches)]
+    print("-")
+    for counter, item in enumerate(matches):
+        print (f"{name}\t{counter}, {item}\t{len(record)}")
     
 
 #print(x[0].start, x[0].matched)
