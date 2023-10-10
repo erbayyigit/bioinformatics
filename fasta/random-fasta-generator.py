ce18fdcbf3740e0e08e769437f59c520055881eb
Erbay Yigit <erbayyigit@gmail.com>
2023-10-09 21:28:13 -0400

    Update random-fasta-generator.py
    converted list dna to tuple and some other small changes

--- fasta/random-fasta-generator.py
+++ fasta/random-fasta-generator.py
@@ -1,9 +1,6 @@
 """
-    yigit@neb.com 2023.10.08
     Generates random fasta file.
-    Number of records given by user.
-    Record length range is giving by user as in k.
-    This script performs well as expected.
+    Number of records and length of each record is given by user.
 """
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -11,17 +8,18 @@ from Bio.SeqRecord import SeqRecord
 import random
 from random import choices
 
-DNA = ['A', 'T', 'C', 'G']
-prob = [0.25, 0.25, 0.25, 0.25] #probability of each base
+
+DNA = ('A', 'T', 'C', 'G')
+prob = [0.1, 0.1, 0.40, 0.40] #probability of each base
 number_of_records = 100 #number of fasta records
+
 
 with open("outputfile.fasta", "w") as output_handle:
     for i in range(number_of_records):
         dna_seq = choices(DNA, prob, k=random.randint(100, 500))
-        dna_seq = ''.join(dna_seq)
-        #Create a SeqRecord object
-        record = SeqRecord(Seq(dna_seq),
-            id="Chowder_"+str(i+1),
+        dna_str = ''.join(dna_seq)
+        record = SeqRecord(Seq(dna_str), #Create a SeqRecord object
+            id=f"Chowder_{i+1}",
             name="Chow-Chow",
             description="random sequence")
         SeqIO.write(record, output_handle, "fasta")
