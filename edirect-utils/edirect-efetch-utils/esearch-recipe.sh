37c06fc50fbe0b8d4542194d8defc6b3b653c9d8
Erbay Yigit <erbayyigit@gmail.com>
2026-02-03 21:35:40 -0500

    rename a bunch of file for better organization

--- edirect-utils/edirect-efetch-utils/esearch-recipe.sh
+++ edirect-utils/edirect-efetch-utils/esearch-recipe.sh
@@ -1,8 +1,9 @@
-
 #Example-1
-#Finde most prolific authors who published on m6A
+#Find most prolific authors who published on m6A
 esearch -db pubmed -query "m6A"|\
 efetch -format xml|\
 xtract -pattern PubmedArticle -block Author \
 -sep " " -tab "\n" -element LastName,Initials|\
 sort-uniq-count-rank
+
+
